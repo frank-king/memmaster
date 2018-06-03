5a8bfb2af9e76acff209bfdd8468b4b1a207bc19
Frank King <frankking1729@gmail.com>
2018-06-03 18:25:12 +0800

    Add the basic app.

---
+++ database/users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS`users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `password` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
